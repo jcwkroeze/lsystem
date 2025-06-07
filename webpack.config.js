dc575e00b0b654f13f7fd89906b38c4afba9b32a
Jan <jcwkroeze@pm.me>
2025-06-07 17:20:56 +0000

    Fixing webpack for dev container

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,44 @@
 const path = require('path');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+module.exports = [
+  {
+    devtool: "source-map",
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name]-bundle.min.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /src[\/\\].*\.ts/,
+          use: 'ts-loader'
+        },
+        {
+          test: /\.(glsl|vs|fs|vert|frag)$/,
+          exclude: /node_modules/,
+          use: [
+            'raw-loader'
+          ]
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".ts"],
+    },
+    watchOptions: {
+      poll: 50,
+      ignored: /node_modules/,
+    },
+    entry: {
+      "main": __dirname + "/src/" + "main.ts",
+    },
+    plugins: [
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        server: { baseDir: ['dist'] },
+      })
+    ]
   },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true
-            }
-          }
-        ],
-        exclude: [/node_modules/, path.resolve(__dirname, 'dist')],
-      },
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: [
-          'raw-loader'
-        ]
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['./dist'] },
-      files: ['./dist/index.html', './dist/style.css' ],
-    }),
-  ],
-};
+]
